ccec2a198bf99b5284d358e1d5b23c62050463cd
vinh <glorious6300@gmail.com>
2019-01-08 17:03:31 +0700

    update a bit about font-error in csv

--- test.js
+++ test.js
@@ -1,9 +1,47 @@
-﻿const fs=require('fs-extra');
-const utf8 = require('utf8');
-string="nguyễn";
-console.log(string.charCodeAt(0));
-console.log(string.charCodeAt(1));
-console.log(string.charCodeAt(2));
-console.log(string.charCodeAt(3));
-console.log(string.charCodeAt(4));
-console.log(string.charCodeAt(5));
+﻿const puppeteer = require('puppeteer');
+const fs=require('fs-extra');
+
+(async function main() {
+    try {
+        const browser = await puppeteer.launch({ headless: false});
+        const page = await browser.newPage();
+  
+        //await page.goto('https://experts.shopify.com/');
+
+        await page.goto('http://10.45.6.12/ipop_webchart/webKadouChart.aspx');
+
+         await page.waitForSelector('#chkKikaiSel');
+         const sections = await page.$$('#chkKikaiSel');
+
+         console.log(sections);
+
+        // for (let i=0; i < sections.length ; i++) {
+        //     await page.goto('https://experts.shopify.com/');
+        //     await page.waitForSelector('.section');
+        //     const sections = await page.$$('.section');
+
+        //     const section =sections[i]
+        //     const button = await section.$('a.marketing-button');
+        //     const buttonName = await page.evaluate( button => button.innerText, button);
+        //     console.log('\n\n');
+        //     console.log(buttonName);
+        //     button.click();
+
+        //     await page.waitForSelector('#ExpertsResults');
+        //     const lis = await page.$$('#ExpertsResults > li');
+
+        //     for (const li of lis){
+        //         const name = await li.$eval('h2', h2 => h2.innerText);
+        //         console.log('name',name);
+                
+        //         //await fs.appendFile('out.csv','section,name\n');
+        //         await fs.appendFile('out.csv', `"${buttonName}","${name}"\n`);
+        //     }
+            
+        // }
+        console.log('done');
+        //await browser.close();
+    }catch(e) {
+        console.log('our erro',e);
+    }
+})();
